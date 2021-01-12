10949a138d2b82a16bc48a9d4832ea6f75c4f43c
Jason Fleming <jfleming9357@gmail.com>
2021-01-12 18:21:14 -0500

    fixed webpack, added server to serve on localhost

---
+++ server.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let app = express();
+
+app.use(express.static('client/dist'));
+
+let port = 3000;
+app.listen(port, ()=> {
+  console.log(`Listening on ${port}` )
+});
