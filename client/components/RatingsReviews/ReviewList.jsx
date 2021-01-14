518b05586ae6d0a6b69ad13d9274a4654bb524bd
Jason Fleming <jfleming9357@gmail.com>
2021-01-13 23:30:10 -0500

    added proxy server, rendered some test data

--- client/components/RatingsReviews/ReviewList.jsx
+++ client/components/RatingsReviews/ReviewList.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-const getReviews = (pId) => {
-  let url = 'http://localhost:3000/api/products';
+const getReviews = (pId, callback) => {
+  let url = 'http://localhost:3000/proxy/api/fec2/hratx/reviews/?product_id=' + pId;
   axios.get(url)
     .then((response) => {
-      return response;
+      callback(response);
     })
     .catch((err) => {
       throw err;
@@ -12,8 +12,28 @@ const getReviews = (pId) => {
 };
 
 export const ReviewList = (props) => {
-  getReviews(props.id);
-  return (<div>
-    Hello from review list
-  </div>);
+  const [loading, setLoading] = useState(true);
+  const [reviews, setReviews] = useState(<div>Loading reviews...</div>);
+  if (loading) {
+    getReviews(props.id, ((data) => {
+      console.log(data.data.results);
+      setLoading(false);
+      setReviews(data.data.results.map((result) => {
+        return (
+          <div key={result.review_id}>{result.summary}</div>
+        );
+      }));
+    }));
+  }
+  return (
+    <div>{reviews}</div>
+  );
+  // console.log(reviews);
+  // return (
+  //   <div>{reviews.map((review) => {
+  //     return (
+  //       <div>{review.summary}</div>
+  //     );
+  //   })}</div>
+  // );
 };
