897e6ffb820d7a16187ab7474ebd9189c24359f9
Jason Fleming <jfleming9357@gmail.com>
2021-01-19 20:56:55 -0500

    added axios calls for helpful/report in reviews

--- client/components/RatingsReviews/handleReviewClicks.jsx
+++ client/components/RatingsReviews/handleReviewClicks.jsx
@@ -1,7 +1,24 @@
-export const helpfulClick = () => {
-  console.log('wow so helpful thanks');
+import axios from 'axios';
+
+export const helpfulClick = (e) => {
+  let url = `http://localhost:3000/proxy/api/fec2/hratx/reviews/${e.target.id}/helpful`;
+  axios.put(url)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
-export const reportClick = () => {
-  console.log('wow this dude is rude');
+export const reportClick = (e) => {
+  console.log('go cry about it');
+  let url = `http://localhost:3000/proxy/api/fec2/hratx/reviews/${e.target.id}/report`;
+  // axios.put(url)
+  //   .then((res) => {
+  //     console.log(res);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 };
