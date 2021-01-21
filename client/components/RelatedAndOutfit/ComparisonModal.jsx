e83563cecf835525ef48e2e7f66815cea0d0cd41
Cayla Cardiff <cayla.cardiff@gmail.com>
2021-01-20 22:47:51 -0600

    Related items carousel is working.

---
+++ client/components/RelatedAndOutfit/ComparisonModal.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import exampleData from './exampleData.js';
+
+export const ComparisonModal = (props) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
--- client/components/RelatedAndOutfit/hooksRelatedItems.jsx
+++ client/components/RelatedAndOutfit/hooksRelatedItems.jsx
@@ -4,6 +4,7 @@ import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-re
 import exampleData from './exampleData.js';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import CardRelated from './CardRelated.jsx';
+import ComparisonModal from './ComparisonModal.jsx';
 
 export const HooksRelatedItems = () => {
   const [relatedProductIds, setRelatedProductIds] = useState([]);
@@ -11,11 +12,27 @@ export const HooksRelatedItems = () => {
 
   const getRelatedProductInfo = () => {
     let relatedProdInfoArr = [];
-    relatedProductIds.map((item, index) => {
-      let url = 'http://localhost:3000/proxy/api/fec2/hratx/products/' + item.toString();
-      axios.get(url)
+    relatedProductIds.map((item) => {
+      let tempObj = {};
+      let url1 = 'http://localhost:3000/proxy/api/fec2/hratx/products/' + item.toString();
+      let url2 = 'http://localhost:3000/proxy/api/fec2/hratx/products/' + item.toString() + '/styles';
+      axios.get(url1)
         .then((results) => {
-          relatedProdInfoArr.push(results.data);
+          tempObj = results.data;
+        })
+        .then(() => {
+          return axios.get(url2)
+          .then((results) => {
+            if (results.data.results[0].photos[0].thumbnail_url) {
+              tempObj.thumbnail = results.data.results[0].photos[0].thumbnail_url;
+            }
+          })
+          .catch((error) => {
+            console.error(error, `OH NOOOOO there was an error getting the thumbnail for product ${item.id}`)
+          })
+        })
+        .then(() => {
+          relatedProdInfoArr.push(tempObj);
           if (relatedProdInfoArr.length === relatedProductIds.length) {
             setRelatedProductInfo(relatedProdInfoArr);
           }
@@ -27,7 +44,7 @@ export const HooksRelatedItems = () => {
   };
 
   useEffect(() => {
-    let url = 'http://localhost:3000/proxy/api/fec2/hratx/products/12013/related';
+    let url = 'http://localhost:3000/proxy/api/fec2/hratx/products/12016/related';
     axios.get(url)
       .then((results) => {
         setRelatedProductIds(results.data);
@@ -41,8 +58,41 @@ export const HooksRelatedItems = () => {
     getRelatedProductInfo();
   }, [relatedProductIds]);
 
-  return (<div onClick={() => {
-  }}>{relatedProductInfo.map((item, index) => {
-      return (<div key={index}>{item.name}</div>);
-    })}</div>);
+  return (
+    <div className="border" style={{ height: '450px', overflow: 'hidden' }}>
+      <CarouselProvider
+        className='c-related-items-carousel'
+        naturalSlideHeight={100}
+        naturalSlideWidth={100}
+        totalSlides={relatedProductInfo.length}
+        visibleSlides={3}
+        >
+        <div>
+          <ButtonBack>Back</ButtonBack>
+        </div>
+        <Slider>
+          {relatedProductInfo.map((product) => (
+            <Slide key={product.id} style={{height:'450px', width:'280px', marginLeft: '7px', marginRight: '7px'}} index={0} onClick={() => console.log(relatedProductInfo)}>
+              <div style={{height:'450px', width:'280px'}}>
+                <div style={{height:'70%', width: '100%', backgroundImage: `url(${(product.thumbnail)})`, backgroundRepeat: 'no-repeat'}}><p style={{color:'yellow', fontSize:'25px', textAlign:'right'}} onClick={() => (<ComparisonModal product={product}/>)}>&#9733;</p>
+                </div>
+                <div style={{height:'30%', width: '100%'}}>
+                  <p className="fs-6 m-0">{product.category}</p>
+                  <p className="fs-6 m-0">{product.name}</p>
+                  <p className="fs-6 m-0">${product.default_price}</p>
+                  <p className="fs-6 m-0">STARS</p>
+                  {/* <p  onClick={() => console.log(relatedProductInfo)} className="fs-6 m-0"></p> */}
+                </div>
+              </div>
+            </Slide>
+          ))}
+        </Slider>
+        <div>
+          <ButtonNext>Next</ButtonNext>
+        </div>
+      </CarouselProvider>
+
+    </div>
+  )
 };
+
