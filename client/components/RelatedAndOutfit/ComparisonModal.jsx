dd7426c3f6414975d448d98666d39133abdfb999
Cayla Cardiff <cayla.cardiff@gmail.com>
2021-01-22 12:55:57 -0600

    Comparison modal is functional.

--- client/components/RelatedAndOutfit/ComparisonModal.jsx
+++ client/components/RelatedAndOutfit/ComparisonModal.jsx
@@ -1,24 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Modal, Form } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import { ProductContext } from '../../context/ProductContext';
 import exampleData from './exampleData.js';
 
-export const ComparisonModal = () => {
+export const ComparisonModal = (props) => {
+  const [show, setShow] = useState(props);
+
+  const handleClose = () => setShow(false);
+
+  const merge = (arr) => {
+    return [...new Set([].concat(...arr))];
+  }
+  var features = merge([props.compProduct.features, props.curProduct.features]);
+  features.unshift(props.compProduct.name);
+  features.unshift(props.curProduct.name);
+
+  console.log('features: ', features)
+
   return (
-      <div
-        onClick={() => {/* CLOSE MODAL */}}
-      >X
-      <Modal
-        show={show}
-        backdrop="static"
-        keyboard={false}>
-        <Modal.Header>
-          <Modal.Title as="p">About the THING</Modal.Title>
-        </Modal.Header>
+    <Modal
+      show={show}
+      onHide={handleClose}
+      keyboard={false}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Choices, choices....</Modal.Title>
+      </Modal.Header>
         <Modal.Body>
-          TABLE HERE
+          <table>
+            <tr>
+              <th>{features[0]}</th>
+              <th>{features[1]}</th>
+            </tr>
+          </table>
         </Modal.Body>
-      </Modal>
-          </div>
+    </Modal>
   );
 }
 
