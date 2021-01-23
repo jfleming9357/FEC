7e042fd5e1c4ced46d2072eaabfbc0f3d51018c5
Daniel He <danielhe.mlp@gmail.com>
2021-01-23 12:31:28 -0600

    batched related items API calls in parallel

--- client/components/RelatedAndOutfit/hooksRelatedItems.js
+++ client/components/RelatedAndOutfit/hooksRelatedItems.js
@@ -27,33 +27,19 @@ export const HooksRelatedItems = () => {
     let relatedProdInfoArr = [];
     relatedProductIds.map((item) => {
       let tempObj = {};
-      let url1 =
-        '/proxy/api/fec2/hratx/products/' +
-        item.toString();
-      let url2 =
-        '/proxy/api/fec2/hratx/products/' +
-        item.toString() +
-        '/styles';
-      axios
-        .get(url1)
-        .then((results) => {
-          tempObj = results.data;
-        })
-        .then(() => {
-          return axios
-            .get(url2)
-            .then((results) => {
-              if (results.data.results[0].photos[0].thumbnail_url) {
-                tempObj.thumbnail =
-                  results.data.results[0].photos[0].thumbnail_url;
-              }
-            })
-            .catch((error) => {
-              console.error(
-                error,
-                `OH NOOOOO there was an error getting the thumbnail for product ${item.id}`
-              );
-            });
+      let urls = [
+        '/proxy/api/fec2/hratx/products/' + item.toString(),
+        '/proxy/api/fec2/hratx/products/' + item.toString() + '/styles'
+      ];
+      Promise.all(urls.map(url => {
+        return axios.get(url)
+          .then(res => res.data);
+      }))
+        .then(data => {
+          tempObj = data[0];
+          if (data[1].results[1].photos[0].thumbnail_url) {
+            tempObj.thumbnail = data[1].results[0].photos[0].thumbnail_url;
+          }
         })
         .then(() => {
           relatedProdInfoArr.push(tempObj);
@@ -67,6 +53,40 @@ export const HooksRelatedItems = () => {
             'OOOPS!  There was an error getting the information about related product.'
           );
         });
+
+      // axios
+      //   .get(url1)
+      //   .then((results) => {
+      //     tempObj = results.data;
+      //   })
+      //   .then(() => {
+      //     return axios
+      //       .get(url2)
+      //       .then((results) => {
+      //         if (results.data.results[0].photos[0].thumbnail_url) {
+      //           tempObj.thumbnail =
+      //             results.data.results[0].photos[0].thumbnail_url;
+      //         }
+      //       })
+      //       .catch((error) => {
+      //         console.error(
+      //           error,
+      //           `OH NOOOOO there was an error getting the thumbnail for product ${item.id}`
+      //         );
+      //       });
+      //   })
+      //   .then(() => {
+      //     relatedProdInfoArr.push(tempObj);
+      //     if (relatedProdInfoArr.length === relatedProductIds.length) {
+      //       setRelatedProductInfo(relatedProdInfoArr);
+      //     }
+      //   })
+      //   .catch((error) => {
+      //     console.error(
+      //       error,
+      //       'OOOPS!  There was an error getting the information about related product.'
+      //     );
+      //   });
     });
   };
 
