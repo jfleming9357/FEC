ce13bed25d2806c78012ad4acf3c5a9d87c8cda6
Daniel He <danielhe.mlp@gmail.com>
2021-02-24 16:43:48 -0600

    forgot to save this

--- client/components/RatingsReviews/SortReviews.js
+++ client/components/RatingsReviews/SortReviews.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, createContext } from 'react';
+import React, { useState, createContext } from 'react';
 export const SortContext = createContext({
   sortMethod: 'relevance',
   setSortMethod: (method) => {}
